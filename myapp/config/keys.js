58bb873b5b71a7974dfcef3f275f7e2c794da9d3
Federico Cremonini <43166141+fedecremo@users.noreply.github.com>
2018-09-19 17:57:21 +0200

    non funziona

---
+++ myapp/config/keys.js
@@ -0,0 +1,14 @@
+// add this file to .gitignore
+
+module.exports = {
+  google: {
+    clientID:'735700976420-su3q2o37fe22qjdmvioe1oal1p003cuf.apps.googleusercontent.com',
+    clientSecret: 'aWDlOSasrV5JixzzpUxtOyJK'
+  },
+  mongodb: {
+    dbURI: 'mongodb://fedecremo:cremo4fede@ds259742.mlab.com:59742/myapp-youtube'
+  },
+  session: {
+    cookieKey: 'iamawesome'
+  }
+};
